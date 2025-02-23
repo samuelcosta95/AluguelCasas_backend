44e0da26a07129fd0b8dcd90c61eedf277c2b826
Samuel Costa <samuelcostalf95@gmail.com>
2025-02-22 22:38:58 -0300

    Preparando para o deploy no Render

--- core/settings.py
+++ core/settings.py
@@ -90,9 +90,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-import dj_database_url
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
